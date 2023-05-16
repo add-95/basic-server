686f7b1bebe428970fd654a7545874a90f213c4c
Atanu Das <atanudas0985@gmail.com>
2023-05-17 01:57:18 +0530

    Creating another Controller and Router

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,8 @@
 module.exports.home = function(req,res){
-    return res.end('<h1>Express is up.</h1>')
+    return res.end('<h1>Home Page</h1>');
 }
-module.exports.homeWeb = function(req,res){
-    return res.end('<h1>Currrently you are in "/home" tab</h1>')
-}
+// module.exports.homeWeb = function(req,res){
+//     return res.end('<h1>Currrently you are in "/home" tab</h1>')
+// }
 
 // module.exports.actionName = function(req,res){}
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const homeController = require('../controllers/home_controller');
 console.log("Router Loaded..");
 
 router.get('/', homeController.home);
-router.get('/home', homeController.homeWeb);
+// router.get('/home', homeController.homeWeb);
+
+router.use('/users', require('./users'));
+// router.use('/routerName', require('./routerfile'));
 
 module.exports = router;
