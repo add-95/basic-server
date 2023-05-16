72255e71d36e2120739bffe3305da6b894c96c62
Atanu Das <atanudas0985@gmail.com>
2023-05-17 02:20:25 +0530

    Rendering our Home Page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,9 @@
 module.exports.home = function(req,res){
-    return res.end('<h1>Home Page</h1>');
+
+    return res.render('home', {
+        title: "HOME"
+    });
 }
-// module.exports.homeWeb = function(req,res){
-//     return res.end('<h1>Currrently you are in "/home" tab</h1>')
-// }
+
 
 // module.exports.actionName = function(req,res){}
